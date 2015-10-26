1b1965e28eff14a335ee30736b12ca732b283e21
Andrea Jemmett <andreajemmett@gmail.com>
2015-10-26 19:20:57 +0100

    created setup and test scripts

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# check that wine is installed
+if [ ! `which wine` ]; then
+	echo 'Please install wine manually first: apt-get install wine'
+	exit 0
+fi
+
+if [ -d fceux ]; then
+	echo 'FCEUX is already installed'
+else
+	# download fceux
+	wget 'http://heanet.dl.sourceforge.net/project/fceultra/Binaries/2.2.2/fceux-2.2.2-win32.zip'
+	# extract it and remove archive
+	unzip fceux-2.2.2-win32.zip -d fceux
+	rm fceux-2.2.2-win32.zip
+fi
+
+# download S.M.B. rom
+wget 'http://23.227.191.210/997ajxXXajs13jJKPxOa/may/120/NES%20Roms/Super%20Mario%20Bros.%20(Japan,%20USA).zip' -O SuperMarioBros.zip
+unzip SuperMarioBros.zip
+rm SuperMarioBros.zip
+mv 'Super Mario Bros. (Japan, USA).nes' roms/SuperMarioBros.nes
+
+echo 'Do you want to run the test now? [y/N]'
+read -n 1 answer
+if [ $answer == 'y' ]; then
+	wine fceux/fceux.exe -lua test.lua roms/SuperMarioBros.nes
+fi
+
---
+++ test.lua
@@ -0,0 +1,4 @@
+while (true) do
+	gui.text(50, 50, 'Hello, world!')
+	emu.frameadvance()
+end
